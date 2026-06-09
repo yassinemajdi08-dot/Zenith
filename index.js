8286241b32d60a663505f259e5ad691a31190d4c
yassinemajdi08-dot <yassinemajdi08@gmail.com>
2026-06-09 04:03:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-const { loadModel, checkImage } = require("./ai/model");
-const checkVideo = require("./ai/videoCheck");
-
-const fetch = require("node-fetch");
-const fs = require("fs");
-const path = require("path");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
-});
-
-let processing = false;
-
-client.once("ready", async () => {
-  console.log("Bot ready");
-  await loadModel();
-});
-
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
-  if (processing) return;
-
-  processing = true;
-
-  try {
-    for (const file of message.attachments.values()) {
-
-      const url = file.url;
-      const type = file.contentType || "";
-
-      const tempPath = path.join(__dirname, "temp_" + Date.now());
-
+console.log("BOT STARTING...");
       const res = await fetch(url);
       const buffer = Buffer.from(await res.arrayBuffer());
       fs.writeFileSync(tempPath, buffer);
