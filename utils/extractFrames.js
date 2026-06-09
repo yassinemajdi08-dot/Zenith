f7dee6edfe66ab7a9e35e4447cc0e48bf766277b
yassinemajdi08-dot <yassinemajdi08@gmail.com>
2026-06-09 01:36:48 +0100

    Update extractFrames.js

--- utils/extractFrames.js
+++ utils/extractFrames.js
@@ -1,13 +1,10 @@
 const ffmpeg = require("fluent-ffmpeg");
-const fs = require("fs");
 
 function extractFrames(videoPath, outputDir) {
   return new Promise((resolve, reject) => {
-    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
-
     ffmpeg(videoPath)
       .screenshots({
-        count: 5,
+        count: 3,
         folder: outputDir,
         filename: "frame-%i.png",
         size: "320x240"
