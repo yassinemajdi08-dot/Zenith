c6230b7529c82e90aa8e081186248e11c23dfc77
yassinemajdi08-dot <yassinemajdi08@gmail.com>
2026-06-09 03:33:58 +0100

    Update extractFrames.js

--- utils/extractFrames.js
+++ utils/extractFrames.js
@@ -6,10 +6,7 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 function extractFrames(videoPath, outputDir) {
   return new Promise((resolve, reject) => {
     ffmpeg(videoPath)
-      .outputOptions([
-        "-vf fps=1",     // frame كل ثانية (خفيف)
-        "-vframes 5"     // فقط 5 frames
-      ])
+      .outputOptions(["-vf fps=1", "-vframes 5"])
       .save(`${outputDir}/frame-%d.png`)
       .on("end", resolve)
       .on("error", reject);
