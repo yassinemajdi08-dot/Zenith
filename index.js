5f14ab49af9dbde377612d52872dc868e66aed39
yassinemajdi08-dot <yassinemajdi08@gmail.com>
2026-06-09 04:37:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { loadModel, checkImage } = require("./ai/model");
+const checkVideo = require("./ai/videoCheck");
+
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+let processing = false;
+
+client.once("ready", async () => {
+  console.log("Bot ready");
+  await loadModel();
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (processing) return;
+
+  processing = true;
+
+  try {
+    for (const file of message.attachments.values()) {
+
+      const url = file.url;
+      const type = file.contentType || "";
+
+      const tempPath = path.join(__dirname, "temp_" + Date.now());
+
+      const res = await fetch(url);
+      const buffer = Buffer.from(await res.arrayBuffer());
+      fs.writeFileSync(tempPath, buffer);
+
+      let score = 0;
+
+      if (type.includes("video")) {
+        score = await checkVideo(tempPath);
+      } else {
+        score = await checkImage(tempPath);
+      }
+
+      if (score >= 0.45) {
+        await message.delete().catch(() => {});
+      }
+
+      fs.unlinkSync(tempPath);
+    }
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+
+  processing = false;
+});
+
+client.login(process.env.TOKEN);
